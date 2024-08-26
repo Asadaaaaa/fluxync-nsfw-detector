f547c432782f6eaeaa9c543e18bd3cac1d53d270
Mikail Asada <62200797+Asadaaaaa@users.noreply.github.com>
2024-08-26 14:24:14 +0700

    feat: api detect-image

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 from predictor import ImagePredictor
+from flask import Flask, request, jsonify
 
-if __name__ == "__main__":
-    # Model config
+app = Flask(__name__)
+
+@app.route("/detect-image", methods=["POST"])
+def predict_image():
+    data = request.get_json()
+    if 'image_path' not in data:
+        return jsonify({"error": "No image path provided"}), 400
+    
+    image_path = data['image_path']
+
     model_path = 'model/fluxync-mobilenetv2.keras'
     predictor = ImagePredictor(model_path)
 
-    # Image config
-    image_path = 'test_images/porn/porn-1.jpg'
     result = predictor.predict_image(image_path)
-
-    print(f"This model predicts the image as: {result['image_label']} with a score of {result['image_score']:.2f}")
+    
+    return jsonify({"image_label": result['image_label'], "image_score": float(result['image_score'])})
+    
+app.run(host='0.0.0.0', port=7300)
